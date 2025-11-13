359af7e19ffed09b427435df891374f26bae9059
Bartek Strach <iambartekstrach@gmail.com>
2025-11-13 14:31:24 +0100

    feat: add desktop and mobile images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
@@ -36,6 +37,24 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            // hidden - remove image from DOM on mobile screens
+            // md:block - show image on desktop screens
+            className='hidden md:block'
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            // block - show image on mobile screens
+            // md:hidden - remove image from DOM on desktop screens
+            className='block md:hidden'
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
